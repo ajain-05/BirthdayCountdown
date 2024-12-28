279f06f5b3bef229afbf00ffb0c73d681ccb3dc9
Ajain Raj Prashar <165676386+ajain-05@users.noreply.github.com>
2024-12-28 12:22:31 +0530

    Create birthday.js

---
+++ birthday.js
@@ -0,0 +1,20 @@
+function BirthdayTimer(){
+    const Days = document.getElementById("day");
+    const Hours = document.getElementById("hour");
+    const Minutes = document.getElementById("minute")
+    const Seconds = document.getElementById("second")
+    const date1 = new Date()
+    const date2 = new Date("2025-10-05T00:00:00")
+    const date = date2-date1;
+    const day = Math.floor(date/(1000*60*60*24));
+    const hour = Math.floor((date/(1000*60*60))%24);
+    const minute = Math.floor((date/(1000*60))%60)
+    const second = Math.floor((date/1000)%60)
+    Days.innerHTML = day;
+    Hours.innerHTML = hour
+    Minutes.innerHTML = minute
+    Seconds.innerHTML = second
+}
+setInterval(BirthdayTimer,1000);
+
+
